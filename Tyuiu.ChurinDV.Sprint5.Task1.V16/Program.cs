0d273c251c851e971803771d71c99756d31d8b2c
ChurinDmitriy <mister.dim2017111@gmail.com>
2023-11-14 21:17:00 +0500

    Task1

---
+++ Tyuiu.ChurinDV.Sprint5.Task1.V16/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ChurinDV.Sprint5.Task1.V16.Lib;
+
+
+namespace Tyuiu.ChurinDV.Sprint5.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Чурин Дмитрий Владимирович | ИСТНб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
